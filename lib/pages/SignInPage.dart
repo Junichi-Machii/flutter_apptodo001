c2db2def819c263109d2411037494770d3469870
Junichi-Machii <ronmachii@gmail.com>
2023-07-26 18:16:13 +0900

    signUp-In 画面作成

---
+++ lib/pages/SignInPage.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'sign In',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              buttonItem(Icons.g_mobiledata, "Continue with Google", 40),
+              SizedBox(
+                height: 15,
+              ),
+              buttonItem(Icons.phone_android, 'Continue with Mobile', 30),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Or',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              textItem('Email...'),
+              SizedBox(
+                height: 15,
+              ),
+              textItem('Password...'),
+              SizedBox(
+                height: 20,
+              ),
+              colorButton(),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "If you don't have an Account? ",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'SignUp',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget colorButton() {
+    return Container(
+      width: MediaQuery.of(context).size.width - 90,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 143, 24, 180),
+          Color.fromARGB(255, 194, 74, 231),
+          Color.fromARGB(255, 143, 24, 180),
+        ]),
+      ),
+      child: Center(
+        child: Text(
+          'sign In',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buttonItem(IconData iconDate, String buttonName, double iconsize) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 60,
+      height: 60,
+      child: Card(
+        color: Colors.black,
+        elevation: 8,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+          side: BorderSide(
+            width: 1,
+            color: Colors.grey,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              iconDate,
+              size: iconsize,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Text(
+              buttonName,
+              style: TextStyle(color: Colors.white, fontSize: 17),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget textItem(String labeltext) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 70,
+      height: 55,
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: labeltext,
+            labelStyle: TextStyle(
+              fontSize: 17,
+              color: Colors.white,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(
+                width: 1,
+                color: Colors.grey,
+              ),
+            )),
+      ),
+    );
+  }
+}
